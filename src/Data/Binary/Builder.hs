a613c5062573b4bd708b106c451b5d6683dea502
Simon Meier <iridcode@gmail.com>
2011-06-09 19:12:15 +0200

    ported Builder to new bytestring builder

--- src/Data/Binary/Builder.hs
+++ src/Data/Binary/Builder.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : Data.Binary.Builder
--- Copyright   : Lennart Kolmodin, Ross Paterson
+-- Copyright   : Lennart Kolmodin, Ross Paterson, Simon Meier
 -- License     : BSD3-style (see LICENSE)
 -- 
 -- Maintainer  : Lennart Kolmodin <kolmodin@dtek.chalmers.se>
@@ -50,4 +50,117 @@ module Data.Binary.Builder (
 
   ) where
 
-import Data.Binary.Builder.Base
+import Data.Monoid
+
+import qualified Data.ByteString      as S
+import qualified Data.ByteString.Lazy as L
+
+import Data.ByteString.Builder
+import Data.ByteString.Builder.Char.Utf8
+
+import Foreign
+
+-- | /O(1)./ The empty Builder, satisfying
+--
+--  * @'toLazyByteString' 'empty' = 'L.empty'@
+--
+{-# INLINE empty #-}
+empty :: Builder
+empty = mempty
+
+-- | /O(1)./ A Builder taking a single byte, satisfying
+--
+--  * @'toLazyByteString' ('singleton' b) = 'L.singleton' b@
+--
+{-# INLINE singleton #-}
+singleton :: Word8 -> Builder
+singleton = word8
+
+-- | /O(1)./ The concatenation of two Builders, an associative operation
+-- with identity 'empty', satisfying
+--
+--  * @'toLazyByteString' ('append' x y) = 'L.append' ('toLazyByteString' x) ('toLazyByteString' y)@
+--
+{-# INLINE append #-}
+append :: Builder -> Builder -> Builder
+append = mappend
+
+-- | /O(1)./ A Builder taking a 'S.ByteString', satisfying
+--
+--  * @'toLazyByteString' ('fromByteString' bs) = 'L.fromChunks' [bs]@
+--
+{-# INLINE fromByteString #-}
+fromByteString :: S.ByteString -> Builder
+fromByteString = byteString
+
+-- | /O(1)./ A Builder taking a lazy 'L.ByteString', satisfying
+--
+--  * @'toLazyByteString' ('fromLazyByteString' bs) = bs@
+--
+{-# INLINE fromLazyByteString #-}
+fromLazyByteString :: L.ByteString -> Builder
+fromLazyByteString = lazyByteString
+
+-- | Write a Word16 in big endian format
+{-# INLINE putWord16be #-}
+putWord16be :: Word16 -> Builder
+putWord16be = bigEndian
+
+-- | Write a Word32 in big endian format
+{-# INLINE putWord32be #-}
+putWord32be :: Word32 -> Builder
+putWord32be = bigEndian
+
+-- | Write a Word64 in big endian format
+{-# INLINE putWord64be #-}
+putWord64be :: Word64 -> Builder
+putWord64be = bigEndian
+
+-- | Write a Word16 in little endian format
+{-# INLINE putWord16le #-}
+putWord16le :: Word16 -> Builder
+putWord16le = littleEndian
+
+-- | Write a Word32 in little endian format
+{-# INLINE putWord32le #-}
+putWord32le :: Word32 -> Builder
+putWord32le = littleEndian
+
+-- | Write a Word64 in little endian format
+{-# INLINE putWord64le #-}
+putWord64le :: Word64 -> Builder
+putWord64le = littleEndian
+
+-- | /O(1)./ A Builder taking a single native machine word. The word is
+-- written in host order, host endian form, for the machine you're on.
+-- On a 64 bit machine the Word is an 8 byte value, on a 32 bit machine,
+-- 4 bytes. Values written this way are not portable to
+-- different endian or word sized machines, without conversion.
+--
+{-# INLINE putWordhost #-}
+putWordhost :: Word -> Builder
+putWordhost = hostEndian
+
+-- | Write a Word16 in native host order and host endianness.
+-- 2 bytes will be written, unaligned.
+{-# INLINE putWord16host #-}
+putWord16host :: Word16 -> Builder
+putWord16host = hostEndian
+
+-- | Write a Word32 in native host order and host endianness.
+-- 4 bytes will be written, unaligned.
+{-# INLINE putWord32host #-}
+putWord32host :: Word32 -> Builder
+putWord32host = hostEndian
+
+-- | Write a Word64 in native host order.
+-- On a 32 bit machine we write two host order Word32s, in big endian form.
+-- 8 bytes will be written, unaligned.
+{-# INLINE putWord64host #-}
+putWord64host :: Word64 -> Builder
+putWord64host = hostEndian
+
+-- | Write a character using UTF-8 encoding.
+{-# INLINE putCharUtf8 #-}
+putCharUtf8 :: Char -> Builder
+putCharUtf8 = utf8
